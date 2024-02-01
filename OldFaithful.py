a9f9b57407b48e136049c7864b8cd04f61f1a0f6
Kulis <131580807+Mattkulis@users.noreply.github.com>
2024-02-01 00:10:08 -0600

    Add files via upload

---
+++ OldFaithful.py
@@ -0,0 +1,24 @@
+##this is the beginning to my backtesting script aimed at locating dip buy opportunities, and times to exit.
+#set variables up here
+# 
+
+
+##Entry: If closing Price(n1) exceeds ALL the closing prices of the prior three candles (n-2),(n-3),(n-4) AND the HIGHS of the prior five candles,(n2),(n3),(n4)(n5) (n6) then Enter.
+##Exit/close: If the  close price of ALL the prior three candle’s(n1),(n2) & (n3) exceed,(>) LIVE current price(n0),  AND current price(n0) is below lows of the prior 5 candles(n2)(n3)(n4)(n5)(n6). 
+
+
+
+
+
+
+def oldFaithful()
+    password = input("please enter password")
+    for letters in password:
+        if(letter.isupper()== True):
+            isvalid = True
+        if(isValid) = True):
+            print("valid")    
+        else:
+    print("invalid")
+
+oldFaithful()
